f904a5097142a6b2cf5b95208d44ed94d585c5a8
Aleksei Aparin <alekseiaparin@gmail.com>
2023-01-27 16:13:01 +0200

    Update

--- 005_functional_programming/practical_test/007_task.py
+++ 005_functional_programming/practical_test/007_task.py
@@ -2,10 +2,18 @@
 #         Примеры:
 #         1234565 seconds = 14:6:56:5
 
-sec = 123423565
-days = int(sec / 86400)
-hours = int(int(sec % 86400) / 3600)
-minutes = int(int(hours % 3600) / 60)
-sec_and = minutes / 60
+# sec = 123423565
+# days = int(sec / 86400)
+# hours = int(int(sec % 86400) / 3600)
+# minutes = int(int(hours % 3600) / 60)
+# sec_and = minutes / 60
+#
+# print(f'{days}:{hours}:{minutes}:{minutes}:{sec_and}')
 
-print(f'{days}:{hours}:{minutes}:{minutes}:{sec_and}')
+sec = 123423565
+days = sec // 86400
+hours = (sec % 86400) // 3600
+minutes = ((sec % 86400) % 3600) // 60
+sec_and = ((sec % 86400) % 3600) % 60
+
+print(f'{sec} seconds = {days}:{hours}:{minutes}:{sec_and}')
